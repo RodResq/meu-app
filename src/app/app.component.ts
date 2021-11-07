db5747749d128d90951473458276ed2bd7c49d91
RodResq <rodrigoresque@yahoo.com.br>
2021-11-07 17:24:21 -0300

    Revisao Input() e Output() properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'meu-app';
+
+  constructor() {
+  }
+
+  handleClick(event) {
+    console.log(event);
+  }
 }
--- src/app/meu-componente/meu-componente.component.ts
+++ src/app/meu-componente/meu-componente.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-meu-componente',
   templateUrl: './meu-componente.component.html',
   styleUrls: ['./meu-componente.component.css']
 })
-export class MeuComponenteComponent implements OnInit {
+export class MeuComponenteComponent implements OnInit, OnDestroy {
+
+  nome = 'Treina Web';
+
+  @Input('nomeExterno') atributoExterno;
+
+  @Output() myClick = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Abc');
   }
 
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+
+  callClick() {
+    this.myClick.emit(123)
+  }
 }
