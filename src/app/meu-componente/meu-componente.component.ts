8b88cf539ba5e7d743a521e1b379ac11f07a3f42
RodResq <rodrigoresque@yahoo.com.br>
2021-11-21 22:30:56 -0300

    revisao diretivas, ngClass

--- src/app/meu-componente/meu-componente.component.ts
+++ src/app/meu-componente/meu-componente.component.ts
@@ -1,18 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-meu-componente',
   templateUrl: './meu-componente.component.html',
   styleUrls: ['./meu-componente.component.css']
 })
-export class MeuComponenteComponent implements OnInit {
+export class MeuComponenteComponent implements OnInit, AfterViewInit {
 
-  myStyle = '';
+  nome = 'Treinaweb';
+  @ViewChild('minhaClasse') minhaClasse: ElementRef;
 
   constructor(
   ) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  recuperarClasseEstilo(element) {
+    if (element == null) {
+      return;
+    }
+    return {
+      classe1: (element.value == 1),
+      classe2: (element.value == 2),
+      classe3: (element.value == 3),
+      classeMaior1: (element.value > 1)
+    };
   }
 }
